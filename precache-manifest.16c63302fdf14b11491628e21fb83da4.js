393e3e75286dda0fcf71b4990cde814dccea125f
spadhi <somyashree.padhi@amadeus.com>
2020-09-17 22:36:28 +0530

    Updates

---
+++ precache-manifest.16c63302fdf14b11491628e21fb83da4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dade4b7829288eedc2c3211361716c6f",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "54695e67fde30c5f2c0e",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "67a6b61e3c815ed25f6c",
+    "url": "/my-app/static/js/2.180d86fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.180d86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54695e67fde30c5f2c0e",
+    "url": "/my-app/static/js/main.c4410804.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
